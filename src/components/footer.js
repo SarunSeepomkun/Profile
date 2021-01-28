eb913b8ce38421b2cec4650f6d95240380e847ce
Sarun Seepomkun <sarunseepomkun@gmail.com>
2021-01-28 17:55:34 +0700

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,16 +6,9 @@ const footer = () => {
       <div className="container flex flex-space-between">
         <div>
           <h1>Sarun Seepomkun</h1>
-          <p>Copyright &copy; 2020</p>
+          <p>Copyright &copy; 2021</p>
         </div>
         <div className="social">
-          <a
-            target="_blank"
-            href="https://github.com/SarunSeepomkun"
-            rel="noopener noreferrer"
-          >
-            <i className="fab fa-github fa-2x"></i>
-          </a>
           <a
             target="_blank"
             href="https://linkedin.com/in/sarun-seepomkun-653051b5"
@@ -23,9 +16,13 @@ const footer = () => {
           >
             <i className="fab fa-linkedin fa-2x"></i>
           </a>
-          {/* <a target="_blank" href="#" rel="noopener noreferrer">
-              <i className="fab fa-line fa-2x"></i>
-            </a> */}
+          <a
+            target="_blank"
+            href="https://github.com/SarunSeepomkun"
+            rel="noopener noreferrer"
+          >
+            <i className="fab fa-github fa-2x"></i>
+          </a>
         </div>
       </div>
     </footer>
